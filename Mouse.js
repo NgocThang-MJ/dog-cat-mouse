5088187fc08b09a091b6fbe21914fdf6df442f35
LiquidJS <tranngocthang.bkdn@gmail.com>
2020-04-12 11:22:00 -0400

    update mouse method

--- Mouse.js
+++ Mouse.js
@@ -1,6 +1,11 @@
 function Mouse(name, weigh) {
 	this.name = name;
 	this.weigh = weigh; 
+	this.isDead = false;
 }
+
+Mouse.prototype.die = function() {
+	this.isDead = true;
+};
 
 module.exports = Mouse;
