6817c2497c58572a7476644b465d5bf96cf135a3
LiquidJS <tranngocthang.bkdn@gmail.com>
2020-04-12 11:19:25 -0400

    Revert "Add die method for Mouse"
    This reverts commit 9b8c4877b38f506173d175afde6ada5d23a50738.

--- Mouse.js
+++ Mouse.js
@@ -1,11 +1,6 @@
 function Mouse(name, weigh) {
 	this.name = name;
 	this.weigh = weigh; 
-	this.dead = false;
 }
-
-Mouse.prototype.die = function() {
-	this.dead = true;
-};
 
 module.exports = Mouse;
